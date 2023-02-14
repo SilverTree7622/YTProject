9522495f08354c6b416cbaab8ecb382b67b9a142
FluxHunter <40912698+SilverTree7622@users.noreply.github.com>
2023-02-14 16:49:22 +0900

    testing(index): import html

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import "./tailwind.css";
 import Layout from "./layout/main";
 
 
+const addFooter = async function () {
+    const resp = await fetch("footer.htm");
+    const html = await resp.text();
+    document.body.insertAdjacentHTML("beforeend", html);
+}
+console.log('addFooter: ', addFooter);
+
+
 const app = document.createElement('div');
 app.id = 'app';
 app.className = 'flex items-center justify-center h-screen';
